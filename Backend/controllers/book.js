edcfeda5b371e02469b5b7fe72891e53d40bd38b
baharahou <dx.vhrh@gmail.com>
2024-02-04 12:47:26 +0100

    get one, best, all Book

--- Backend/controllers/book.js
+++ Backend/controllers/book.js
@@ -22,3 +22,22 @@ exports.createBook = (req, res) => {
       res.status(400).json({ error });
     });
 };
+
+exports.getAllBooks = (req, res) => {
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+};
+exports.getOneBook = (req, res) => {
+  Book.findOne({ _id: req.params.id })
+    .then((book) => res.status(200).json(book))
+    .catch((error) => res.status(404).json({ error }));
+};
+
+exports.getBestBook = (req, res) => {
+  Book.find()
+    .sort({ averageRating: -1 })
+    .limit(3)
+    .then((books) => res.status(200).json(books))
+    .catch((error) => res.status(400).json({ error }));
+};
--- Backend/routes/book.js
+++ Backend/routes/book.js
@@ -5,5 +5,8 @@ const bookCtrl = require("../controllers/book");
 const router = express.Router();
 
 router.post("/", auth, multer, bookCtrl.createBook);
+router.get("/", bookCtrl.getAllBooks);
+router.get("/:id", bookCtrl.getOneBook);
+router.get("/bestrating", bookCtrl.getBestBook);
 
 module.exports = router;
